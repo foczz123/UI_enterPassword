6f5fc72c25b7affe8db6f1e196a99b337bb7ec8c
Supakorn Yookack <foczza@gmail.com>
2022-01-29 20:59:07 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'game.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Guess The Number',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.green,
       ),
       home: HomePage(),
     );
@@ -23,155 +20,121 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
   final _controller = TextEditingController();
-  late Game _game;
-
-  HomePage({Key? key}) : super(key: key) {
-    _game = Game(maxRandom: 100);
-  }
-
-  void _showOkDialog(BuildContext context, String title, String content) {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(title),
-          content: Text(content),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('GUESS THE NUMBER'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+      body: SizedBox.expand(
         child: Container(
-          decoration: BoxDecoration(
-            color: Colors.purple.shade50,
-            borderRadius: BorderRadius.circular(16.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.purple.shade100,
-                offset: Offset(5.0, 5.0),
-                spreadRadius: 2.0,
-                blurRadius: 5.0,
-              )
-            ],
-          ),
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              /*Row(
+          decoration: BoxDecoration(color: Colors.greenAccent),
+          child: Padding(
+            padding: const EdgeInsets.all(100),
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  Container(width: 50.0, height: 50.0, color: Colors.blue),
                   Expanded(
-                    child: Container(
-                      width: 30.0,
-                      height: 50.0,
-                      //color: Colors.pink,
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: Text('FLUTTER', textAlign: TextAlign.end,),
-                      ),
-                      alignment: Alignment.centerRight,
-                    ),
-                  ),
-                  //SizedBox(width: 10.0),
-                ],
-              ),*/
-              /*Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Expanded(child: Container(color: Colors.green, width: 100.0, height: 50.0)),
-                  Container(color: Colors.red, width: 50.0, height: 50.0),
-                ],
-              ),*/
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset('assets/images/guess_logo.png', width: 90.0),
-                    SizedBox(width: 8.0),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text('GUESS',
-                            style: TextStyle(
-                                fontSize: 36.0, color: Colors.purple.shade200)),
-                        Text(
-                          'THE NUMBER',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.purple.shade600,
-                            //fontWeight: FontWeight.bold,
-                          ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.lock,
+                              size: 100.0,
+                              color: Colors.blueAccent,
+                            ),
+                            Text(
+                              'กรุณาใส่รหัสผ่าน',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 25.0,
+                                  color: Colors.black),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  controller: _controller,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white.withOpacity(0.7),
-                    border: OutlineInputBorder(),
-                    hintText: 'ทายเลขตั้งแต่ 1 ถึง 100',
                   ),
-                ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for (var i = 1; i <= 3; i++) buildButton(i),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for (var i = 4; i <= 6; i++) buildButton(i),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for (var i = 7; i <= 9; i++) buildButton(i),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 65.0,
+                        height: 65.0,
+                      ),
+                      Container(
+                          width: 75.0, height: 75.0, child: buildButton(0)),
+                      Container(
+                        width: 65.0,
+                        height: 65.0,
+                        child: Icon(
+                          Icons.backspace,
+                          size: 55.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextButton(
+                      child: Text('ลืมรหัสผ่าน',
+                          style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20.0)),
+                      style: TextButton.styleFrom(
+                        primary: Colors.black,
+
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: ElevatedButton(
-                  child: Text('GUESS'),
-                  onPressed: () {
-                    var input = _controller.text;
-                    var guess = int.tryParse(input);
-
-                    if (guess == null) {
-                      _showOkDialog(context, 'ERROR',
-                          'กรอกข้อมูลไม่ถูกต้อง ให้กรอกเฉพาะตัวเลขเท่านั้น');
-                      return;
-                    }
-
-                    late String message;
-                    var guessResult = _game.doGuess(guess);
-                    if (guessResult > 0) {
-                      message = '$guess มากเกินไป กรุณาลองใหม่';
-                    } else if (guessResult < 0) {
-                      message = '$guess น้อยเกินไป กรุณาลองใหม่';
-                    } else {
-                      message =
-                          '$guess ถูกต้องครับ 🎉\n\nคุณทายทั้งหมด ${_game.guessCount} ครั้ง';
-                    }
-
-                    _showOkDialog(context, 'RESULT', message);
-                  },
-                ),
-              ),
-            ],
+            ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildButton(int num) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 60.0,
+        height: 60.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: Colors.black, width: 3.0),
+          shape: BoxShape.circle,
+        ),
+        child: Center(
+          child: Text('$num', style: TextStyle(
+              fontSize: 25.0, color: Colors.black),),
         ),
       ),
     );
